6bfd63ff0800054d8eaa9539bb8bbc2a1c8e629a
Ринат Рафиков <rafikov864@yandex.ru>
2024-12-13 02:13:41 +0500

    add: turn orders, new dictionary; fix: ?word check, !bluff calls check

--- main.py
+++ main.py
@@ -5,31 +5,40 @@ with open("words.txt", "r") as word_file:
 print(f"{len(words)} words loaded")
 p1 = HumanPlayer("Rinat")
 p2 = PrimitiveAI()
+move_order = input(f"Who moves first?\n(0) {p1.name} (1) {p2.name}: ")
+if (move_order == "1"):
+    players = [p2, p1]
+else:
+    players = [p1, p2]
 turn_counter = 0
 current_string = ""
 while (True):
-    if turn_counter % 2 == 0:
-        current_player = p1
-    else:
-        current_player = p2
-    print(f"_{current_string}_")
-    inp = current_player.play(current_string, words)
+    current_p_index = turn_counter % 2
+    past_p_index = (turn_counter + 1) % 2
+    print(f"Current string: _{current_string}_")
+    inp = players[current_p_index].play(current_string, words)
     if inp[0] == ">":
         current_string += inp[1]
     elif inp[0] == "<":
         current_string = inp[1] + current_string
     elif inp[0] == "?":
         print(inp[1:] in words)
+        continue
     elif inp[0] == "!":
-        if current_string in words:
-            print("bluff call was INCORRECT")
-            print(f"Player {current_player.name} lost :P")
+        print(f"{players[current_p_index].name} called a bluff")
+        if any([current_string in word for word in words]):
+            print(f"bluff call was INCORRECT")
+            winner = players[past_p_index]
+            #print(f"Player {players[current_p_index].name} lost :P")
         else:
-            print("bluff call was correct")
-            print(f"Player {current_player.name} won :)")
+            print(f"bluff call was correct")
+            winner = players[current_p_index]
+            #print(f"Player {players[current_p_index].name} won :)")
         break
     if current_string in words and len(current_string) > 3:
-        print(f"word spelled: {current_string}")
-        print(f"Player {current_player.name} lost :P")
+        print(f"{players[current_p_index].name} spelled the word: {current_string}")
+        winner = players[past_p_index]
+        #print(f"Player {players[current_p_index].name} lost :P")
         break
     turn_counter += 1
+print(f"{winner.name} WON!")
