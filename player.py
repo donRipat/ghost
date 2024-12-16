4f692b603bd519505f3ea0998e5b949d19eb8c03
Ринат Рафиков <rafikov864@yandex.ru>
2024-12-17 02:17:55 +0500

    ughhh: fail fix accident short word spells by AI

--- player.py
+++ player.py
@@ -23,14 +23,23 @@ class PrimitiveAI(Player):
         chosen_word = ""
         for word in words:
             if ss in word and len(word) > len(chosen_word):
+                words_substrings = [word[i:j] for i in range(len(word))
+                                    for j in range(i + 1, len(word) + 1)]
+                if any([ss in words and ss != word for ss in words_substrings]):
+                    continue
                 chosen_word = word
         index = chosen_word.find(ss)
         move = ""
-        if index > 0:
-            move = f"<{chosen_word[index-1]}"
-        elif index != -1:
-            move = f">{chosen_word[index+len(ss)]}"
-        else:
-            move = "!"
+        try:
+            if index > 0:
+                move = f"<{chosen_word[index-1]}"
+            elif index != -1:
+                move = f">{chosen_word[index+len(ss)]}"
+            else:
+                move = "!"
+                print(f"chosen_word={chosen_word},\nindex={index},\nss={ss}")
+        except:
+            print(f"chosen_word={chosen_word},\nindex={index},\nss={ss}")
+            raise Exception("ligma")
         print(f"{self.name}'s move: {move}")
         return move
