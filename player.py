3d743e9a06480942fa5581462709de5e563056a9
rinat <deez@nut.com>
2024-12-20 02:13:23 +0500

    add: i created a monster 0_0

--- player.py
+++ player.py
@@ -12,7 +12,7 @@ class HumanPlayer(Player):
         ...
 
     def play(self, ss, words):
-        return input(f"{self.name}'s move: ")
+        return input()
 
 
 class PrimitiveAI(Player):
@@ -20,26 +20,51 @@ class PrimitiveAI(Player):
         self.name = "Dummy"
 
     def play(self, ss, words):
-        chosen_word = ""
+        candidates = []
         for word in words:
-            if ss in word and len(word) > len(chosen_word):
-                words_substrings = [word[i:j] for i in range(len(word))
-                                    for j in range(i + 1, len(word) + 1)]
-                if any([ss in words and ss != word for ss in words_substrings]):
-                    continue
-                chosen_word = word
-        index = chosen_word.find(ss)
-        move = ""
-        try:
-            if index > 0:
-                move = f"<{chosen_word[index-1]}"
-            elif index != -1:
-                move = f">{chosen_word[index+len(ss)]}"
-            else:
-                move = "!"
-                print(f"chosen_word={chosen_word},\nindex={index},\nss={ss}")
-        except:
-            print(f"chosen_word={chosen_word},\nindex={index},\nss={ss}")
-            raise Exception("ligma")
-        print(f"{self.name}'s move: {move}")
+            if ss in word:
+                candidates.append(word)
+        if len(candidates) == 0:
+            return "!"
+        chosen_word = ""
+        # print(f"candidates={candidates[:50]}")
+        for word in candidates:
+            index = word.find(ss)
+            if len(word) > index+len(ss):
+                if ss + word[index+len(ss)] not in words:
+                    # print(f"word={word}")
+                    return f">{word[index+len(ss)]}"
+            elif word[index-1] + ss not in words:
+                # print(f"word={word}")
+                return f"<{word[index-1]}"
+        return self.__bluff(ss)
+
+    def __bluff(self, ss):
+        import random as rd
+        if rd.choice([True, False]):
+            neighbor_letter = ss[:-1]
+            move = ">"
+        else:
+            neighbor_letter = ss[0]
+            move = "<"
+        if move == ">":
+            file = "top_last_letters"
+        else:
+            file = "top_first_letters"
+        with open(file, "r") as fr:
+            lines = fr.read().split()
+        letter_weight = {}
+        for line in lines:
+            letter, weight = line.split(":")
+            if letter != neighbor_letter:
+                letter_weight[letter] = weight
+        # print(letter_weight)
+        population=list(letter_weight.keys())
+        weights=[float(x) for x in letter_weight.values()]
+        # print(population)
+        # print(weights)
+        move += rd.choices(population=population,
+                           weights=weights,
+                           k=1)[0]
         return move
+
