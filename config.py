c2c0bb75ec8ba1d6f472c919ff3a3c5b68628893
Ilia Ezhov <javantlang@gmail.com>
2024-12-21 01:21:50 +0500

    fix: fixed top_letters

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 with open("src/resources/words_ge_4.txt", "r") as word_file:
     WORDS = frozenset([str(word) for word in word_file.read().split()])
     print(f"{len(WORDS)} words loaded")
+
+with open("src/resources/top_first_letters", "r") as file:
+    Top_First_Letters = file.read().split()
+
+with open("src/resources/top_last_letters", "r") as file:
+    Top_Last_Letters = file.read().split()
