05e53fe4f063af74989098dba8f6e205321e2633
Ilia Ezhov <javantlang@gmail.com>
2024-12-21 13:41:17 +0500

    fix: added config.yml, random move order, upper letter fix, user default name fix feat: pass move

---
+++ src/services/moves/pass_move.py
@@ -0,0 +1,9 @@
+from src.interfaces.IMove import IMove
+
+
+class PassMove(IMove):
+    def execute(self):
+        pass
+
+    def cancel(self):
+        pass
--- src/services/players/primitive_ai.py
+++ src/services/players/primitive_ai.py
@@ -6,49 +6,36 @@ from src.interfaces import IPlayer
 
 class PrimitiveAI(IPlayer):
     def __init__(self, name: str | None = None):
-        super().__init__("Dummy" if name is None else name)
+        super().__init__(name or "Dummy")
 
     def play(self, ss, words):
         candidates = [word for word in words if ss in word]
         if len(candidates) <= 0:
             return "!"
-        chosen_word = ""
-        # print(f"candidates={candidates[:50]}")
+
         for word in candidates:
             index = word.find(ss)
-            if len(word) > index+len(ss):
-                if ss + word[index+len(ss)] not in words:
-                    # print(f"word={word}")
-                    return f">{word[index+len(ss)]}"
-            elif word[index-1] + ss not in words:
-                # print(f"word={word}")
+            if len(word) > index+len(ss) and ss + word[index+len(ss)] not in words:
+                return f">{word[index+len(ss)]}"
+            elif index != 0 and word[index-1] + ss not in words:
                 return f"<{word[index-1]}"
         return self.__bluff(ss)
 
-    def __bluff(self, ss):
+    @staticmethod
+    def __bluff(ss):
         if rd.choice([True, False]):
-            neighbor_letter = ss[:-1]
+            neighbor_letter = ss[-1]
             move = ">"
+            letter_weight = {k: v for k, v in Top_Last_Letters.items() if k != neighbor_letter}
         else:
             neighbor_letter = ss[0]
             move = "<"
-        if move == ">":
-            lines = Top_Last_Letters
-        else:
-            lines = Top_First_Letters
-        letter_weight = {}
-        for line in lines:
-            letter, weight = line.split(":")
-            if letter != neighbor_letter:
-                letter_weight[letter] = weight
-        # print(letter_weight)
-        population=list(letter_weight.keys())
-        weights=[float(x) for x in letter_weight.values()]
-        # print(population)
-        # print(weights)
+            letter_weight = {k: v for k, v in Top_First_Letters.items() if k != neighbor_letter}
+
+        population = list(letter_weight.keys())
         move += rd.choices(
             population=population,
-            weights=weights,
+            weights=list(letter_weight.values()),
             k=1
         )[0]
         return move
